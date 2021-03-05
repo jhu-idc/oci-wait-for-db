30467492c9416d6cef70efb8d1c4b0df175dec18
Derek Belrose <dbelrose@jhu.edu>
2021-03-05 13:56:37 -0500

    Remove hardcoded namedspace and k8s domain

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,5 +2,5 @@
 
 MYSQL_HOST=${MYSQL_HOST:-mariadb}
 
-until nslookup ${MYSQL_HOST}.$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace).svc.cluster.local; do echo waiting for ${MYSQL_HOST}; sleep 2; done
+until nslookup ${MYSQL_HOST}; do echo waiting for ${MYSQL_HOST}; sleep 2; done
 
